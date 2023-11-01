1c385d27a3fbe1bb6caf44af29992f4de661bea0
Faizal_Husain_Adiasha <adihusain0@gmail.com>
2023-11-01 09:30:16 +0700

    fix : signin page

--- src/pages/auth/signin/index.tsx
+++ src/pages/auth/signin/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import {
-  Grid,
-  Stack,
-  Text,
-  createStyles,
-  Image,
-  TextInput,
-  PasswordInput,
-  Box,
-  Button,
+    Grid,
+    Stack,
+    Text,
+    createStyles,
+    Image,
+    TextInput,
+    PasswordInput,
+    Box,
+    Button,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import Link from "next/link";
@@ -19,32 +19,32 @@ import { cookies } from "next/headers";
 import Cookies from "js-cookie";
 
 const useStyles = createStyles((theme) => ({
-  wrapper: {
-    background: "#EEF0F6",
-  },
-  title: {
-    [theme.fn.smallerThan("lg")]: {
-      fontSize: "60px",
+    wrapper: {
+        background: "#EEF0F6",
     },
-  },
-  loginFormLayout: {
-    [theme.fn.smallerThan("md")]: {
-      width: "300px",
+    title: {
+        [theme.fn.smallerThan("lg")]: {
+            fontSize: "60px",
+        },
     },
-  },
-  hiddenMobile: {
-    [theme.fn.smallerThan("sm")]: {
-      display: "none",
+    loginFormLayout: {
+        [theme.fn.smallerThan("md")]: {
+            width: "300px",
+        },
     },
-  },
-  imageAuth: {},
+    hiddenMobile: {
+        [theme.fn.smallerThan("sm")]: {
+            display: "none",
+        },
+    },
+    imageAuth: {},
 }));
 
 const API = process.env.API_URL;
 
 export default function Index() {
     const Router = useRouter();
-    const { classes } = UseStyles();
+    const { classes } = useStyles();
     const form = useForm({
         initialValues: {
             email: "",
@@ -73,83 +73,95 @@ export default function Index() {
         }
     };
 
-  return (
-    <>
-      <div className={classes.wrapper}>
-        <Grid mb={0}>
-          <Grid.Col pb={0} sm={6} md={5} sx={{ width: "100%" }}>
-            <Stack sx={{ height: "100vh" }} align="center" justify="center">
-              <Box sx={{ width: "405px" }} className={classes.loginFormLayout}>
-                <Text
-                  c="primary"
-                  fw={600}
-                  size={45}
-                  align="center"
-                  className={classes.title}
-                >
-                  Selamat Datang !
-                </Text>
-                <Text c="black" fw={500} size={18} align="center">
-                  Silahkan Masuk Untuk Memulai Sesi Anda
-                </Text>
-                <br />
-                <TextInput
-                  size="lg"
-                  radius={11}
-                  label="Email"
-                  {...form.getInputProps("email")}
-                />
-                <PasswordInput
-                  mt="md"
-                  size="lg"
-                  radius={11}
-                  label="Password"
-                  {...form.getInputProps("password")}
-                />
-                <br />
-                <Link href="forgot-password">
-                  <Text
-                    c="primary"
-                    align="left"
-                    sx={{
-                      ":hover": {
-                        fontWeight: 500,
-                      },
-                    }}
-                  >
-                    Forgot Password?
-                  </Text>
-                </Link>
+    return (
+        <>
+            <div className={classes.wrapper}>
+                <Grid mb={0}>
+                    <Grid.Col pb={0} sm={6} md={5} sx={{ width: "100%" }}>
+                        <Stack
+                            sx={{ height: "100vh" }}
+                            align="center"
+                            justify="center"
+                        >
+                            <Box
+                                sx={{ width: "405px" }}
+                                className={classes.loginFormLayout}
+                            >
+                                <Text
+                                    c="primary"
+                                    fw={600}
+                                    size={45}
+                                    align="center"
+                                    className={classes.title}
+                                >
+                                    Selamat Datang !
+                                </Text>
+                                <Text
+                                    c="black"
+                                    fw={500}
+                                    size={18}
+                                    align="center"
+                                >
+                                    Silahkan Masuk Untuk Memulai Sesi Anda
+                                </Text>
+                                <br />
+                                <TextInput
+                                    size="lg"
+                                    radius={11}
+                                    label="Email"
+                                    {...form.getInputProps("email")}
+                                />
+                                <PasswordInput
+                                    mt="md"
+                                    size="lg"
+                                    radius={11}
+                                    label="Password"
+                                    {...form.getInputProps("password")}
+                                />
+                                <br />
+                                <Link href="forgot-password">
+                                    <Text
+                                        c="primary"
+                                        align="left"
+                                        sx={{
+                                            ":hover": {
+                                                fontWeight: 500,
+                                            },
+                                        }}
+                                    >
+                                        Forgot Password?
+                                    </Text>
+                                </Link>
 
-                <br />
-                <Button
-                  variant="filled"
-                  size="lg"
-                  radius="md"
-                  fullWidth
-                  onClick={handleSubmit}
-                >
-                  Masuk
-                </Button>
-              </Box>
-            </Stack>
-          </Grid.Col>
-          <Grid.Col
-            pb={0}
-            sm={6}
-            md={7}
-            sx={{ width: "100%" }}
-            className={classes.hiddenMobile}
-          >
-            <Image
-              alt="auth"
-              src="/AuthImage.png"
-              height="100vh"
-              className=""
-            />
-          </Grid.Col>
-        </Grid>
-      </div>
-    </>
-  );
+                                <br />
+                                <Button
+                                    variant="filled"
+                                    size="lg"
+                                    radius="md"
+                                    fullWidth
+                                    onClick={handleSubmit}
+                                >
+                                    Masuk
+                                </Button>
+                            </Box>
+                        </Stack>
+                    </Grid.Col>
+                    <Grid.Col
+                        pb={0}
+                        sm={6}
+                        md={7}
+                        sx={{ width: "100%" }}
+                        className={classes.hiddenMobile}
+                    >
+                        <Image
+                            alt="auth"
+                            src="/AuthImage.png"
+                            height="100vh"
+                            className=""
+                        />
+                    </Grid.Col>
+                </Grid>
+            </div>
+        </>
+    );
 }
