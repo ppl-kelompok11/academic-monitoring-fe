161e03f173d3416da5d52f56835c2bc4bedb831c
Faizal_Husain_Adiasha <adihusain0@gmail.com>
2023-11-08 12:33:42 +0700

    fix : add irs input

--- src/pages/academic/irs/[id]/edit/index.tsx
+++ src/pages/academic/irs/[id]/edit/index.tsx
@@ -1,7 +1,164 @@
-import React from 'react'
-import { useRouter } from 'next/router'
+import React, { useEffect } from "react";
+import {
+    Stack,
+    Text,
+    createStyles,
+    Box,
+    TextInput,
+    Group,
+    Button,
+    Space,
+    NativeSelect,
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
+import Link from "next/link";
+import AppLayout from "@/layouts/AppLayout";
+import { IoIosArrowBack } from "react-icons/io";
+import next from "next";
+import FileUpload from "@/components/molekul/FileUpload";
+import api from "@/configs/axios-interceptors";
+import { useRouter } from "next/router";
+import { useCallback } from "react";
+
+const UseStyles = createStyles((theme) => ({
+    wrapper: {},
+    form: {
+        background: "#FFFFFF",
+        borderRadius: "10px",
+    },
+    title: {
+        [theme.fn.smallerThan("lg")]: {
+            fontSize: "60px",
+        },
+    },
+    addMahasiswaFormLayout: {
+        [theme.fn.smallerThan("md")]: {
+            width: "300px",
+        },
+    },
+    hiddenMobile: {
+        [theme.fn.smallerThan("sm")]: {
+            display: "none",
+        },
+    },
+}));
 
 export default function Index() {
-  const router = useRouter()
-  return <p>target edited id Irs: {router.query.id}</p>
+    const { classes } = UseStyles();
+    const router = useRouter();
+    const form = useForm({
+        initialValues: {
+            semester: "",
+            sks: "",
+            scan_irs: {
+                path: "",
+                filename: "",
+                ext: "",
+                url: "",
+            },
+        },
+    });
+    useEffect(() => {
+        if (router.isReady) {
+            console.log(router.query.id);
+            showDetail(router.query.id);
+        }
+    }, [router.isReady]);
+
+    const showDetail = async (id: any) => {
+        try {
+            const response = await api.get(`/irs/${id}`);
+            if (response.status === 200) {
+                form.setValues({
+                    semester: response.data.semester,
+                    sks: response.data.sks,
+                    scan_irs: response.data.scan_irs,
+                });
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleSubmit = async () => {
+        try {
+            const response = await api.post("/irs", {
+                semester: form.values.semester,
+                sks: form.values.sks,
+                scan_irs: form.values.scan_irs.path,
+            });
+
+            if (response.status === 200) {
+                router.push("/academic/irs");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+
+        console.log(form.values);
+    };
+    const handleUpload = (file: any) => {
+        form.setFieldValue("scan_irs", file);
+        console.log(form.values);
+    };
+
+    return (
+        <AppLayout activeLink="academic" role="mahasiswa">
+            <div className={classes.wrapper}>
+                <Stack mx={45}>
+                    <Group spacing={5}>
+                        <Box
+                            component={Link}
+                            href="/mahasiswa/dashboard"
+                            display="flex"
+                            style={{ textDecoration: "none" }}
+                        >
+                            <IoIosArrowBack size={32} />
+                        </Box>
+                        <Text c="black" size={32} fw={700} align="left">
+                            Input IRS
+                        </Text>
+                    </Group>
+                    <Box className={classes.form} py={15} px={20}>
+                        <form
+                            onSubmit={form.onSubmit((values) =>
+                                console.log(values)
+                            )}
+                        >
+                            <TextInput
+                                size="md"
+                                label="Semester"
+                                {...form.getInputProps("semester")}
+                            />
+                            <Space h={15} />
+                            <TextInput
+                                size="md"
+                                label="Jumlah SKS Diambil"
+                                {...form.getInputProps("sks")}
+                            />
+                            <Space h={15} />
+                            <Text
+                                c="primary"
+                                size={18}
+                                fw={500}
+                                align="left"
+                                mb={5}
+                            >
+                                Scan IRS
+                            </Text>
+                            <FileUpload
+                                file={form.values.scan_irs}
+                                // onFileUpload={handleUpload}
+                            />
+                            <Group mt="md">
+                                <Button type="submit" onClick={handleSubmit}>
+                                    Simpan
+                                </Button>
+                            </Group>
+                        </form>
+                    </Box>
+                </Stack>
+            </div>
+        </AppLayout>
+    );
 }
--- src/pages/academic/irs/index.tsx
+++ src/pages/academic/irs/index.tsx
@@ -1,7 +1,248 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import {
+    createStyles,
+    Table,
+    ScrollArea,
+    rem,
+    Card,
+    Grid,
+    Text,
+    Input,
+    Flex,
+    Button,
+    ActionIcon,
+    Badge,
+    Pagination,
+    Stack,
+    Tabs,
+    Space,
+    Center,
+    Group,
+    Box,
+} from "@mantine/core";
+import {
+    IconEditCircle,
+    IconInfoCircle,
+    IconSearch,
+    IconTrash,
+} from "@tabler/icons-react";
+import AppLayout from "@/layouts/AppLayout";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { IoIosArrowBack } from "react-icons/io";
+import api from "@/configs/axios-interceptors";
+import { useCallback } from "react";
+import { GetServerSideProps } from "next";
+import Cookies from "js-cookie";
+import getConfig from "next/config";
+import TitleWithBack from "@/components/atoms/TitleWithBack";
+// const { publicRuntimeConfig } = getConfig();
+const useStyles = createStyles((theme) => ({
+    header: {
+        position: "sticky",
+        top: 0,
+        backgroundColor:
+            theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
+        transition: "box-shadow 150ms ease",
 
-export default function Index() {
-  return (
-    <div>List IRS</div>
-  )
-}
+        "&::after": {
+            content: '""',
+            position: "absolute",
+            left: 0,
+            right: 0,
+            bottom: 0,
+            borderBottom: `${rem(1)} solid ${
+                theme.colorScheme === "dark"
+                    ? theme.colors.dark[3]
+                    : theme.colors.gray[2]
+            }`,
+        },
+    },
+
+    scrolled: {
+        boxShadow: theme.shadows.sm,
+    },
+}));
+
+// type TableScrollAreaProps = NextPageWithAuth & {
+//   data: {
+//     id: number;
+//     order_name: string;
+//     customer_name: string;
+//     instantion: string;
+//     quantity: number;
+//     total_price: number;
+//   }[];
+// };
+
+const Mahasiswa = () => {
+    const router = useRouter();
+    const token = Cookies.get("token");
+    const [activePage, setPage] = useState(1);
+    const [search, setSearch] = useState("");
+    const [totalPage, setTotalPage] = useState(0);
+    const [data, setData] = useState([]);
+    const { classes, cx } = useStyles();
+    const [scrolled, setScrolled] = useState(false);
+
+    const getData = useCallback(async () => {
+        try {
+            const response = await api.get(
+                `irs?search=${search}&page=${activePage}`
+            );
+            console.log(response.data.data);
+            setData(response.data.data);
+            setTotalPage(response.data.meta.last_page);
+        } catch (error) {
+            console.log(error);
+        }
+    }, [search, activePage]);
+
+    useEffect(() => {
+        getData();
+    }, [search, activePage]);
+
+    const handleDelete = async (id: number) => {
+        try {
+            const response = await api.delete(`customers/delete?id=${id}`, {
+                headers: { Authorization: `Bearer ${token}` },
+            });
+            console.log(response.data.data);
+            getData();
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const rows = data.map((row: any) => (
+        <tr key={row.id}>
+            <td>{row.semester}</td>
+            <td>{row.sks}</td>
+            <td>{row.scan_irs}</td>
+            <td>{row.created_at}</td>
+            <td>
+                {
+                    <>
+                        <Flex gap="xs">
+                            {/* <ActionIcon
+                                variant="filled"
+                                color="blue"
+                                onClick={() => {
+                                    router.push(`/mahasiswa/detail/${row.id}`);
+                                }}
+                            >
+                                <IconInfoCircle size="1rem" />
+                            </ActionIcon> */}
+                            <ActionIcon
+                                variant="filled"
+                                color="yellow"
+                                onClick={() => {
+                                    router.push(`/academic/irs/${row.id}/edit`);
+                                }}
+                            >
+                                <IconEditCircle size="1rem" />
+                            </ActionIcon>
+                            <ActionIcon
+                                variant="filled"
+                                color="red"
+                                onClick={() => {
+                                    handleDelete(row.id);
+                                }}
+                            >
+                                <IconTrash size="1rem" />
+                            </ActionIcon>
+                        </Flex>
+                    </>
+                }
+            </td>
+        </tr>
+    ));
+
+    return (
+        <AppLayout role="operator" activeLink="accounts">
+            <Stack mt={35} mx={45}>
+                <TitleWithBack
+                    title="Manajemen Akun"
+                    route="/dashboard/operator"
+                />
+                <Card mt={10} bg={"white"} radius={"lg"}>
+                    <Tabs
+                        color="primary"
+                        variant="pills"
+                        value="irs"
+                        onTabChange={(value) =>
+                            router.push(`/academic/${value}`)
+                        }
+                    >
+                        <Tabs.List>
+                            <Tabs.Tab value="irs">IRS</Tabs.Tab>
+                            <Tabs.Tab value="khs">KHS</Tabs.Tab>
+                            <Tabs.Tab value="pkl">PKL</Tabs.Tab>
+                            <Tabs.Tab value="skripsi">Skripsi</Tabs.Tab>
+                        </Tabs.List>
+                    </Tabs>
+                    <Space h={15} />
+                    <Grid justify="space-between">
+                        <Grid.Col md={9} xs={12}>
+                            {/* <Flex gap="md">
+                                <Input
+                                    icon={<IconSearch />}
+                                    placeholder="Cari Mahasiswa"
+                                    radius={8}
+                                    w={300}
+                                    onChange={(e) => {
+                                        setSearch(e.currentTarget.value);
+                                    }}
+                                />
+                            </Flex> */}
+                        </Grid.Col>
+                        <Grid.Col md={3} xs={12}>
+                            <Flex
+                                justify={{ xs: "flex-start", md: "flex-end" }}
+                            >
+                                <Button
+                                    onClick={() => {
+                                        router.push("academic/irs/input");
+                                    }}
+                                >
+                                    Tambah
+                                </Button>
+                            </Flex>
+                        </Grid.Col>
+                    </Grid>
+                    <ScrollArea
+                        mt={10}
+                        onScrollPositionChange={({ y }) => setScrolled(y !== 0)}
+                    >
+                        <Table miw={700}>
+                            <thead
+                                className={cx(classes.header, {
+                                    [classes.scrolled]: scrolled,
+                                })}
+                            >
+                                <tr>
+                                    <th>Semester</th>
+                                    <th>SKS</th>
+                                    <th>Scan IRS</th>
+                                    <th>Dibuat</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>{rows}</tbody>
+                        </Table>
+                        <Center>
+                            <Pagination
+                                my={20}
+                                value={activePage}
+                                onChange={setPage}
+                                total={totalPage}
+                            />
+                        </Center>
+                    </ScrollArea>
+                </Card>
+            </Stack>
+        </AppLayout>
+    );
+};
+
+export default Mahasiswa;
